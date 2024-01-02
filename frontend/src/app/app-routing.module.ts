d8f3fb4162c34dcc1ce7b58c04eeabd1213b98c9
Drew Layton <dlayton66@gmail.com>
2024-01-02 15:28:09 -0600

    feat: Select company page layout

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SelectCompanyComponent } from './select-company/select-company.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "select-company", component: SelectCompanyComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
