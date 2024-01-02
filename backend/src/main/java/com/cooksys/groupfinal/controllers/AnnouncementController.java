c8248dc0d2c27373d6a320f8d8e0932d76b7f2e1
RyanT524 <rjt524@vt.edu>
2024-01-02 14:45:21 -0500

    added createAnnouncement for POST /admin/announcement

--- backend/src/main/java/com/cooksys/groupfinal/controllers/AnnouncementController.java
+++ backend/src/main/java/com/cooksys/groupfinal/controllers/AnnouncementController.java
@@ -1,8 +1,10 @@
 package com.cooksys.groupfinal.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.cooksys.groupfinal.dtos.AnnouncementDto;
 import com.cooksys.groupfinal.services.AnnouncementService;
 
 import lombok.RequiredArgsConstructor;
@@ -14,4 +16,8 @@ public class AnnouncementController {
 	
 	private final AnnouncementService announcementService;
 
+	@PostMapping("/admin/announcements")
+	public void createAnnouncement(@RequestBody AnnouncementDto announcementDto) {
+	    announcementService.createAnnouncement(announcementDto);
+	}
 }
--- backend/src/main/java/com/cooksys/groupfinal/services/impl/AnnouncementServiceImpl.java
+++ backend/src/main/java/com/cooksys/groupfinal/services/impl/AnnouncementServiceImpl.java
@@ -1,13 +1,36 @@
 package com.cooksys.groupfinal.services.impl;
 
 import org.springframework.stereotype.Service;
-
+import com.cooksys.groupfinal.dtos.AnnouncementDto;
+import com.cooksys.groupfinal.exceptions.BadRequestException;
+import com.cooksys.groupfinal.exceptions.NotAuthorizedException;
+import com.cooksys.groupfinal.mappers.AnnouncementMapper;
+import com.cooksys.groupfinal.repositories.AnnouncementRepository;
 import com.cooksys.groupfinal.services.AnnouncementService;
-
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AnnouncementServiceImpl implements AnnouncementService {
+    
+    private final AnnouncementMapper announcementMapper;
+    private final AnnouncementRepository announcementRepository;
+    @Override
+    public void createAnnouncement(AnnouncementDto announcementDto) {
+
+        if(!announcementDto.getAuthor().isAdmin()) {
+            throw new NotAuthorizedException("You must be an Admin to create an Announcement"); 
+        }
+        else if (announcementDto.getMessage() == null || announcementDto.getTitle() == null) {
+            throw new BadRequestException("Please provide a message and a title.");
+            
+        }
+        else {
+            announcementRepository.saveAndFlush(announcementMapper.dtoToEntity(announcementDto));
+        }
+        
+    }
+    
+    
 
 }
