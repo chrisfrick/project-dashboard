62f80bbc433ddb8dcd28ebc051e6ecb245343748
RyanT524 <rjt524@vt.edu>
2024-01-03 14:29:28 -0500

    updated paths

--- backend/src/main/java/com/cooksys/groupfinal/controllers/AnnouncementController.java
+++ backend/src/main/java/com/cooksys/groupfinal/controllers/AnnouncementController.java
@@ -24,12 +24,12 @@ public class AnnouncementController {
 	    announcementService.createAnnouncement(id, announcementDto);
 	}
 	
-	@PutMapping("/company/{announcementId}")
+	@PutMapping("/{announcementId}")
 	public AnnouncementDto updateAnnouncement(@PathVariable("id") Long id, @RequestBody AnnouncementDto announcementDto) {
 	    return announcementService.updateAnnouncement(id, announcementDto);
 	}
 	
-	@DeleteMapping("/company/{announcementId}")
+	@DeleteMapping("/{announcementId}")
 	public AnnouncementDto deleteAnnouncement(@PathVariable("id") Long id) {
 	    return announcementService.deleteAnnouncement(id);
 	}
