88b725d96474dacc51989295a91c7cadfab96644
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2024-01-04 11:54:31 -0500

    add logged-in user and admin check to UserRegistry

--- frontend/src/app/user-registry/user-registry.component.ts
+++ frontend/src/app/user-registry/user-registry.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Router } from '@angular/router';
 
 interface UserData {
   name: string;
@@ -21,17 +23,59 @@ interface NewUser {
 @Component({
   selector: 'app-user-registry',
   templateUrl: './user-registry.component.html',
-  styleUrls: ['./user-registry.component.css']
+  styleUrls: ['./user-registry.component.css'],
 })
 export class UserRegistryComponent implements OnInit {
-  dummyData: UserData[] = [  // CHANGE TO CALL GET REQUEST FOR USERS
-    { name: 'Chris Purnell', email: 'asdf@gmail.com', team: 'Mcdon', active: true, admin: false, status: 'JOINED' },
-    { name: 'Frank Fournier', email: 'eopiorogm@gmail.com', team: 'popeyes', active: false, admin: true, status: 'PENDING' },
-    { name: 'Will Marttala', email: 'hrthr@gmail.com', team: '', active: false, admin: false, status: 'PENDING' },
-    { name: 'Helena Makendegue', email: 'hh35rthhrthrth@gmail.com', team: 'hererherherherhre', active: true, admin: true, status: 'PENDING' },
+  dummyData: UserData[] = [
+    // CHANGE TO CALL GET REQUEST FOR USERS
+    {
+      name: 'Chris Purnell',
+      email: 'asdf@gmail.com',
+      team: 'Mcdon',
+      active: true,
+      admin: false,
+      status: 'JOINED',
+    },
+    {
+      name: 'Frank Fournier',
+      email: 'eopiorogm@gmail.com',
+      team: 'popeyes',
+      active: false,
+      admin: true,
+      status: 'PENDING',
+    },
+    {
+      name: 'Will Marttala',
+      email: 'hrthr@gmail.com',
+      team: '',
+      active: false,
+      admin: false,
+      status: 'PENDING',
+    },
+    {
+      name: 'Helena Makendegue',
+      email: 'hh35rthhrthrth@gmail.com',
+      team: 'hererherherherhre',
+      active: true,
+      admin: true,
+      status: 'PENDING',
+    },
   ];
 
+  constructor(private dataService: DataService, private router: Router) {}
+
   ngOnInit() {
+    this.dataService.currentUser.subscribe((user) => {
+      // Check for logged-in user
+      if (!user) {
+        this.router.navigateByUrl('/login');
+        return;
+      }
+      // Disallow non-admin access
+      if (!user.admin) {
+        this.router.navigateByUrl('/announcements');
+      }
+    });
   }
 
   // overlay visibility
@@ -60,19 +104,19 @@ export class UserRegistryComponent implements OnInit {
   };
   // add user to database/update table
   submitUser(): void {
-    console.log('NAME: ', this.newUser.firstName, this.newUser.lastName)
-    console.log('EMAIL: ', this.newUser.email)
-    console.log('PASSWORD: ', this.newUser.password, this.newUser.confirmPassword)
-    console.log('IS ADMIN: ', this.newUser.admin)
+    console.log('NAME: ', this.newUser.firstName, this.newUser.lastName);
+    console.log('EMAIL: ', this.newUser.email);
+    console.log(
+      'PASSWORD: ',
+      this.newUser.password,
+      this.newUser.confirmPassword
+    );
+    console.log('IS ADMIN: ', this.newUser.admin);
     this.closeAddUserOverlay();
   }
 
   // some sort of delete user function
-  deleteUser(user: UserData): void {
+  deleteUser(user: UserData): void {}
 
-  }
-
-  editUser(user: UserData): void {
-
-  }
+  editUser(user: UserData): void {}
 }
